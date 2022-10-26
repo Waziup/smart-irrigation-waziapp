15519bba424c8930b44fbd57815710115e134787
Solomon Githu <87617849+SolomonGithu@users.noreply.github.com>
2022-10-26 10:44:08 +0300

    Restored sensor meta field

--- app.py
+++ app.py
@@ -409,8 +409,8 @@ def intel_irris_sensor_config():
 				response = requests.get(url, headers=WaziGate_headers)
 				sensor_data = response.json()
 				sensorName = sensor_data['name']
-				#sensorKind = sensor_data['meta']['kind']
-				sensorKind = sensor_data['kind']
+				sensorKind = sensor_data['meta']['kind']
+				#sensorKind = sensor_data['kind']
 				
 				#---------------------#
 				#-- Manage notifying user to add new config --#
